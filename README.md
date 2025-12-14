89238cb7608abd614d2ff3d1f13b376ed6276757
adamus444 <adamus444@customer.mlnnita1.pop.starlinkisp.net>
2025-12-15 00:18:10 +0100

    jus a beginning

---
+++ README.md
@@ -0,0 +1,6 @@
+# OpenGL Project
+
+# Build
+bash
+cmake --preset debug
+cmake --build --preset debug
---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include "test.h"
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int remove(int x, int y)
+{
+    return x - y;
+}
